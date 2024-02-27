28daec78362f930e93f08507ec8711227683f24d
LindaRodriguezSa <linda.rodriguez12@hotmail.com>
2024-02-27 15:17:33 -0500

    Actualizando app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector("h1");
+/* definir titulo */
+titulo.innerHTML = "Juego del numero secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Indica un numero del 1 al 10";
+
